f5ba9e2088378bcc3322c395357a1cbb6da381b9
amcook <amcook127@gmail.com>
2020-11-27 13:49:11 -0400

    collaborating codes

--- lobsterMovement26Nov.R
+++ lobsterMovement26Nov.R
@@ -7,7 +7,6 @@ library(reshape2)
 # The function needs the x and y coordinate of the lobster
 # adding trap location makes it generic
 distanceToTrapCalculator<- function(xLobster, yLobster,xtrap=5, ytrap=5){
-  
     distanceToTrap<- sqrt((xLobster - xtrap)^2 + (yLobster -ytrap)^2)
     return(distanceToTrap)
 }
@@ -24,18 +23,18 @@ randomMove<- function(xLobster, yLobster, dStep){
     
     # Check if the lobster is located outside of the grid
     # I think its ok for them to move outside the grid, so might not be necessary to have this step
-    if(xNew < 0 ){
-      xNew <- 0
-    }
-    if(xNew > 10 ){
-      xNew <- 10
-    }
-    if(yNew < 0 ){
-      yNew <- 0
-    }
-    if(yNew > 10 ){
-      yNew <- 10
-    }
+    #if(xNew < 0 ){
+    #  xNew <- 0
+    #}
+    #if(xNew > 10 ){
+    #  xNew <- 10
+    #}
+    #if(yNew < 0 ){
+    #  yNew <- 0
+    #}
+    #if(yNew > 10 ){
+    #  yNew <- 10
+    #}
     
     return( list(EASTING = xNew, NORTHING = yNew) )
 }
@@ -44,68 +43,62 @@ randomMove<- function(xLobster, yLobster, dStep){
 #if it is within bait influence
 # The function needs the current x and y coordinate of lobster + dStep + distance of the lobster from the bait 
 #to determine if the condition for directionalMove is met
+    #S = 0.993
+    #atTime=0
+    #ZoI = radius_of_influence =1
+    #for each updated time step
+    #ZoI = ZoI * S 
 
 directionalMove<- function(xLobster, yLobster, dStep, distanceToTrap, xtrap=5, ytrap=5,radius_of_influence=15,ZoI=0){
   # Calculating tethaT, !!!!!!!! the concept of angle to trap (ask Adam!)
   #it looks like the quadrant of lobster matters here?
   #AMC - it shouldn't matter, I would just use the bearing function from geosphere package to get thetaT
   
-  if (xLobster < 5){  # for lobsters in 2nd and 3rd quarter
-    degree<- (5 - xLobster)/ distanceToTrap
-    tethaT<- acos(( (degree * pi) / 180 ) )
-  }
-  if (xLobster > 5){  # for lobsters in 1st and 4th quarter( not sure!!!)
-    degree<- (xLobster - 5)/ distanceToTrap
-    tethaT<- 180 - acos( (degree * pi) / 180 ) 
-  }
+  #if (xLobster < 5){  # for lobsters in 2nd and 3rd quarter
+  #  degree<- (5 - xLobster)/ distanceToTrap
+  #  tethaT<- acos(( (degree * pi) / 180 ) )
+  #}
+  #if (xLobster > 5){  # for lobsters in 1st and 4th quarter( not sure!!!)
+  #  degree<- (xLobster - 5)/ distanceToTrap
+  #  tethaT<- 180 - acos( (degree * pi) / 180 ) 
+  #}
+  thetaT = bearing(c(xLobster,yLobster),c(xtrap,ytrap))
   
   # Calculating tethaR
-  b <- 0.1 + (0.9 * distanceToTrap) / radius_of_influence #Radius of influence
-  if( b > 1){
-    b < - 1
-  }
-  
-  u          <- runif( n = 1, min = 0, max = 1 )
-  sign       <- sample( c(1,-1), size = 1, replace = TRUE)
-  tethaR     <- sign * 180 * u^(1/b)  ##AMC I think we need to talk about thetaR, this is not how I interpreted the paper
-  
-  AMCthetaR =F
-  if(AMCthetaR){
     #ZoI is adjusted each time step for decay (shrinkage) of radius of influence (equation 3)
     b = 1 + 0.9 * (distanceToTrap - ZoI) / radius_of_influence
-    theta_R = -180:180
-    P = 1/(180^b) * abs(tethaT) ^ b
+    thetaR = -180:180
+    P = 1/(180^b) * abs(thetaR) ^ b
     Prtheta_R = (1-P) / sum(1-P)
-    theta_r = sample(theta_R,size = 1, prob=Prtheta_R)  
-  }
+    theta_r = sample(thetaR,size = 1, prob=Prtheta_R)  
   
   
-   tetha      <- tethaT + tethaR
+   tetha      <- thetaT + theta_r
   
   xNew   <- dStep * sin(tetha * pi / 180) + xLobster
   yNew   <- dStep * cos(tetha * pi / 180) + yLobster
   
   # Check if the lobster is going to be outside of the grid
   #AMC again I think its ok for lobsters to move outside grid
-  if(xNew < 0 ){
-    xNew <- 0
-  }
-  if(xNew > 10 ){
-    xNew <- 10
-  }
-  if(yNew < 0 ){
-    yNew <- 0
-  }
-  if(yNew > 10 ){
-    yNew <- 10
-  }
+  #if(xNew < 0 ){
+  #  xNew <- 0
+  #}
+  #if(xNew > 10 ){
+  #  xNew <- 10
+  #}
+  #if(yNew < 0 ){
+  #  yNew <- 0
+  #}
+  #if(yNew > 10 ){
+  #  yNew <- 10
+  #}
   
   
   return( list(EASTING = xNew, NORTHING = yNew) )
   
 }
 # A function to get the coordinate at time t, update the coordinate, and return the coordinate at time t + 1
-updateGrid    = function(lobsterCoordinates){
+updateGrid    = function(lobsterCoordinates,radius_of_influence=15, dstep = 5){
   
   # Takes the argument lobsterCoordinates as the x and y coordinates and updates it accordingly
   numberOfLobsters <- nrow(lobsterCoordinates)
@@ -119,12 +112,12 @@ updateGrid    = function(lobsterCoordinates){
     yOld <- lobsterCoordinates[lobsterIndex,2]
     distanceToTrap <- distanceToTrapCalculator(xLobster = xOld,yLobster = yOld)
     
-    if( distanceToTrap > 15){
-      temp <- randomMove(xLobster = xOld , yLobster = yOld , dStep = 5)
+    if( distanceToTrap > radius_of_influence){
+      temp <- randomMove(xLobster = xOld , yLobster = yOld , dStep = dstep)
       xNew[lobsterIndex] <- temp$EASTING
       yNew[lobsterIndex] <- temp$NORTHING
     }else{
-      temp <- directionalMove(xLobster = xOld , yLobster = yOld , distanceToTrap = distanceToTrap, dStep = 5)
+      temp <- directionalMove(xLobster = xOld , yLobster = yOld , distanceToTrap = distanceToTrap, dStep = dstep)
       xNew[lobsterIndex] <- temp$EASTING
       yNew[lobsterIndex] <- temp$NORTHING
     }
@@ -148,7 +141,7 @@ rpoisD<-function (n, lambda,D=1) {
 #density per grid 100 grids
 # each element in the vector represents the starting value in each grid
 initialGrid = rpoisD(n=100,lambda=.1, D = 3)
-
+LobsterStart = data.frame(EASTING = rep(1:10,times=10), NORTHING = rep(1:10,each=10), Lobs = initialGrid)
 
 # setting up Grid extents 
 xmin=0 
@@ -166,10 +159,10 @@ values(simulated_raster)= initialGrid # initial density of lobsters is given to 
 x_centres=xFromCol(simulated_raster)
 y_centres=yFromRow(simulated_raster)
 
-# Select some random points
+# Select some random points for traps
 random_point_count=0
-random_point_sample_number=sum(initialGrid) # the number of points we want based on initial grid
-
+random_point_sample_number=7 # the number of points we want based on initial grid
+random_points = NULL
 while (random_point_count < random_point_sample_number){
   
   easting_random=sample(x_centres, 1)
@@ -190,7 +183,8 @@ while (random_point_count < random_point_sample_number){
 
 #AMC need to include the catching process
 
-initialxyCoordinate <- random_points
+#initialxyCoordinate <- subset(LobsterStart,Lobs>0)
+initialxyCoordinate  = random_points
 coordinatesOverTime <- list()
 coordinatesOverTime[[1]] <- initialxyCoordinate #AMC not sure why this and not the output of simulated_raster
 for(t in 2:100){
